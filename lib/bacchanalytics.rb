bd64abe100cb323611fa5461167a35be842600cb
Javier Vidal <zanaguara@gmail.com>
2010-03-11 21:18:20 +0100

    rack middleware application working

--- lib/bacchanalytics.rb
+++ lib/bacchanalytics.rb
@@ -1,1 +1,15 @@
-# Bacchanalytics
+class Bacchanalytics
+  def initialize(app, options = {})
+    @app = app
+    @web_property_id = options[:web_property_id] || "UA-XXXXX-X"
+  end
+
+  def call(env)
+    status, headers, response = @app.call(env)
+    if headers["Content-Type"].include? "text/html"
+      [status, headers, "<!-- #{@web_property_id} -->\n" + response.body]
+    else
+      [status, headers, response]
+    end
+  end
+end
