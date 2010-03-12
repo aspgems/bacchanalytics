f41b6f6f23c6f85e549a8f1340279c34218bfb0e
Javier Vidal <zanaguara@gmail.com>
2010-03-12 10:30:51 +0100

    minimalist version working

--- lib/bacchanalytics.rb
+++ lib/bacchanalytics.rb
@@ -1,4 +1,5 @@
 class Bacchanalytics
+
   def initialize(app, options = {})
     @app = app
     @web_property_id = options[:web_property_id] || "UA-XXXXX-X"
@@ -6,10 +7,38 @@ class Bacchanalytics
 
   def call(env)
     status, headers, response = @app.call(env)
-    if headers["Content-Type"].include? "text/html"
-      [status, headers, "<!-- #{@web_property_id} -->\n" + response.body]
+    if !headers["Content-Type"].nil? && (headers["Content-Type"].include? "text/html")
+      body = response.body
+      body.sub! /<[bB][oO][dD][yY]>/, "<body>\n\n#{google_analytics_tracking_code}"
+      headers["Content-Length"] = body.length.to_s
+      [status, headers, body]
     else
       [status, headers, response]
     end
   end
+
+  # Construct the new asynchronous version of the Google Analytics code.
+  # http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
+  def google_analytics_tracking_code
+
+    code = <<-HTML
+    <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '#{@web_property_id}']);
+    _gaq.push(['_trackPageview()']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+    })();
+
+    </script>
+    HTML
+
+    return code
+
+  end
+
 end
